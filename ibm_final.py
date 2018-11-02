20d6043b083cea881cbf2ba81c4807cbc1b57c64
Miles Lilly <mileslilly2@gmail.com>
2018-11-01 21:29:22 -0500

    Add files via upload

---
+++ ibm_final.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# <p> This is a notebook for course 9 of the IBM data science track </p>
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
